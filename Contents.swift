d43ddc9e8faf17640ea6c1e6b31e283ba49f1ccd
MacBookPro <macbookpro@MacBookProui-MacBook-Pro.local>
2018-05-30 14:57:59 +0900

    스마트 컨트랙트 기능

--- Contents.swift
+++ Contents.swift
@@ -1,27 +1,56 @@
-//: Playground - noun: a place where people can play
-
-/*
- 
- 
- 
- */
-
-
 
 import Cocoa
+
+//거래가 발생할때 거래 타입 선택 가능
+enum TransactionType : String, Codable{
+    case domestic
+    case international
+}
+
+// 스마트 컨트랙드 프로토콜 함수 - 지원,신청하는 계약임
+protocol SmartContract{
+    func apply(transaction : Transaction)
+}
+
+//스마트 컨트랙트를 상속한 함수 구현
+class TransactionTypeSmartContract : SmartContract{
+    //스마트 컨트랙트 함수가 호출되면 apply 함수 실행
+    func apply(transaction: Transaction) {
+        
+        var fees = 0.0
+        //거래 타입에 따라 fees 계산해주기
+        switch transaction.transactionType {
+        case .domestic:
+            fees = 0.02
+        case .international:
+            fees = 0.05
+        }
+        //거래 타입에 따라서 항목 계산 후 대입
+        transaction.fees = transaction.amount * fees
+        transaction.amount -= transaction.fees
+    }
+}
+
+
 
 //거래 데이터 구조체
 class Transaction : Codable{
     var from: String
     var to: String
     var amount: Double
+    
+    //수수료
+    var fees: Double = 0.0
+    //거래 타입
+    var transactionType : TransactionType
+    
     //초기화
-    init(from:String,to:String,amount:Double) {
+    init(from:String,to:String,amount:Double,transactionType:TransactionType) {
         self.from = from
         self.to = to
         self.amount = amount
+        self.transactionType = transactionType
     }
-    
 }
 
 //블록 구조체
@@ -31,9 +60,10 @@ class Block: Codable{
     var previousHash : String = ""
     var hash: String!
     var nonce : Int
-
+    
     //거래 내역을 가지고 있다.
     private (set) var transactions : [Transaction] = [Transaction]()
+    
     
     //key 변수
     var key : String {
@@ -50,7 +80,7 @@ class Block: Codable{
     func addTransaction(transaction: Transaction){
         self.transactions.append(transaction)
     }
-
+    
     //초기화
     init(){
         self.nonce = 0
@@ -62,13 +92,22 @@ class Blockchain : Codable {
     //블록이 배열형태로 저장됨
     private (set) var blocks : [Block] = [Block]()
     
+    //스마트 계약 관련 데이터 배열 변수
+    private (set) var smartContracts : [SmartContract] = [TransactionTypeSmartContract()]
+    
+    
     //초기화
     init(genesisBlock : Block){
         addBlock(genesisBlock)
     }
     
+    private enum CodingKeys : CodingKey {
+        case blocks
+    }
+    
+
     //블록체인 구조체에 블록을 추가하는 함수
-     func addBlock(_ block : Block){
+    func addBlock(_ block : Block){
         //체인에 아무런 블록이 없으면 제네시스 블록 생성
         if self.blocks.isEmpty{
             //제네시스 블록의 이전 해쉬값 (임의지정)
@@ -76,6 +115,14 @@ class Blockchain : Codable {
             //제네시스 블록의 해쉬값
             block.hash = generateHash(for :block)
         }
+        
+        // run the smart contracts
+        self.smartContracts.forEach { contract in
+            block.transactions.forEach { transaction in
+                contract.apply(transaction: transaction)
+            }
+        }
+        
         //삽입
         self.blocks.append(block)
     }
@@ -123,24 +170,29 @@ let genesisBlock = Block()
 let blockchain = Blockchain(genesisBlock: genesisBlock)
 
 //새로운 거래 발생
-let transaction = Transaction(from: "kim", to: "hong", amount: 1000)
+let transaction = Transaction(from: "kim", to: "hong", amount: 1000,transactionType:.international)
 
 print("--------------------------------------------------------")
 //위에서 발생한 거래내역을 새로운 블럭을 생성하는 함수안에 넣어주었다.
 let block = blockchain.getNextBlock(transactions: [transaction])
 //새로만든 블록을 넣어주고
 blockchain.addBlock(block)
+
+
+//인코딩
+let data = try! JSONEncoder().encode(blockchain)
+//json데이터를 string으로 출력
+let blockchainJSON = String(data: data, encoding: .utf8)
+
+print(blockchainJSON!)
+
 //블록의 총 개수를 확인해보자.
-print(blockchain.blocks.count)
+//print(blockchain.blocks.count)
 
 
 //let block1 = Block()
 //block1.addTransaction(transaction: transaction)
 //block1.key
-
-
-
-
 
 
 // String 확장
